687b01c8815f402d06342aec0fd3ea5f0589d90b
Kaustubh Joshi <90269829+kaustubh-joshi1@users.noreply.github.com>
2021-12-09 15:44:37 +0530

    Update App.js - added 10 emojis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
+import React, { useState } from "react";
 import "./styles.css";
 
+const emojiDictionary = {
+  "😷": "corona mask",
+  "😔": "sad",
+  "❤️": "love",
+  "😊": "Smiling",
+  "😑": "annoyance",
+  "😳": "disbelief",
+  "😉": "Winking face",
+  "😇": "Smiling Face with Halo",
+  "😛": "Face with Tongue",
+  "😋": "Face Savoring Food"
+};
+
+const emojis = Object.keys(emojiDictionary);
+
 export default function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState("translation will appear here..");
+
+  function changeHandler(event) {
+    const inputEmoji = event.target.value;
+    setEmoji(inputEmoji);
+
+    if (inputEmoji in emojiDictionary) {
+      setMeaning(emojiDictionary[inputEmoji]);
+    } else {
+      setMeaning("failure to recognise this emoji");
+    }
+  }
+
+  function emojiClickHandler(inputEmoji) {
+    setMeaning(emojiDictionary[inputEmoji]);
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>inside outttttt</h1>
+      <input
+        onChange={changeHandler}
+        value={emoji}
+        placeholder={"put an emoji here to know the meaning"}
+        style={{
+          padding: "1em",
+          minWidth: "80%"
+        }}
+      />
+      <h2> {emoji} </h2>
+      <h3> {meaning} </h3>
+      {emojis.map((emoji) => (
+        <span
+          onClick={() => emojiClickHandler(emoji)}
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+        >
+          {" "}
+          {emoji}{" "}
+        </span>
+      ))}
     </div>
   );
 }
